c406b61fe55d908aaa90cbf231aaacd47b87c95e
nbytr <51024479+nbytr@users.noreply.github.com>
2023-08-18 16:01:48 +0100

    UI done for the setup screen

--- client/src/tlca-setupwindow.c
+++ client/src/tlca-setupwindow.c
@@ -5,7 +5,16 @@ struct _TlcaSetupWindow
   GtkApplicationWindow parent_instance;
 
   GtkBuilder *builder;
-  GtkLabel *test_label;
+
+  GtkGrid *root_grid;
+
+  GtkLabel *connstatus_label;
+
+  GtkEntry *server_addr_entry;
+  GtkEntry *nickname_entry;
+
+  GtkButton *connect_button;
+
 };
 
 G_DEFINE_TYPE (TlcaSetupWindow, tlca_setup_window, GTK_TYPE_APPLICATION_WINDOW);
@@ -20,12 +29,17 @@ tlca_setup_window_init (TlcaSetupWindow *self)
 {
   self->builder = gtk_builder_new_from_resource ("/org/tlca/client/ui/setup-window.ui");
 
-  self->test_label = GTK_LABEL (gtk_builder_get_object (self->builder, "test_label"));
+  self->root_grid = GTK_GRID (gtk_builder_get_object (self->builder, "root_grid"));
   
+  self->connstatus_label = GTK_LABEL (gtk_builder_get_object (self->builder, "connstatus_label"));
+
+  self->server_addr_entry = GTK_ENTRY (gtk_builder_get_object (self->builder, "server_addr_entry"));
+  self->nickname_entry = GTK_ENTRY (gtk_builder_get_object (self->builder, "nickname_entry"));
+
+  self->connect_button = GTK_BUTTON (gtk_builder_get_object (self->builder, "connect_button"));
+
   gtk_window_set_title (GTK_WINDOW (self), "Connect");
-  gtk_window_set_default_size (GTK_WINDOW (self), 300, 200);
-  
-  gtk_window_set_child (GTK_WINDOW (self), GTK_WIDGET (self->test_label));
+  gtk_window_set_child (GTK_WINDOW (self), GTK_WIDGET (self->root_grid));
 }
 
 TlcaSetupWindow *
