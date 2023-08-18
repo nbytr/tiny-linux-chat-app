6268380d3bdb6f1dd5e3a1476cfd35fe8e6e11ab
nbytr <51024479+nbytr@users.noreply.github.com>
2023-08-18 12:35:20 +0100

    Setup GResource with GtkBuilder to load .ui files

--- client/src/tlca-setupwindow.c
+++ client/src/tlca-setupwindow.c
@@ -3,6 +3,9 @@
 struct _TlcaSetupWindow
 {
   GtkApplicationWindow parent_instance;
+
+  GtkBuilder *builder;
+  GtkLabel *test_label;
 };
 
 G_DEFINE_TYPE (TlcaSetupWindow, tlca_setup_window, GTK_TYPE_APPLICATION_WINDOW);
@@ -15,8 +18,14 @@ tlca_setup_window_class_init (TlcaSetupWindowClass *klass)
 static void
 tlca_setup_window_init (TlcaSetupWindow *self)
 {
+  self->builder = gtk_builder_new_from_resource ("/org/tlca/client/ui/setup-window.ui");
+
+  self->test_label = GTK_LABEL (gtk_builder_get_object (self->builder, "test_label"));
+  
   gtk_window_set_title (GTK_WINDOW (self), "Connect");
   gtk_window_set_default_size (GTK_WINDOW (self), 300, 200);
+  
+  gtk_window_set_child (GTK_WINDOW (self), GTK_WIDGET (self->test_label));
 }
 
 TlcaSetupWindow *
